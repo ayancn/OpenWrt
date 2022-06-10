90acd628f96c7a2f82cfc2d1ee4942f392872815
ayancn <96911869+ayancn@users.noreply.github.com>
2022-06-11 00:37:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,10 +14,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-store kenzok/luci-app-store
-svn co https://github.com/immortalwrt/packages/trunk/net/dns2socks immortalwrt/dns2socks
-svn co https://github.com/immortalwrt/packages/trunk/net/microsocks immortalwrt/microsocks
-svn co https://github.com/immortalwrt/packages/trunk/net/ipt2socks immortalwrt/ipt2socks
-svn co https://github.com/immortalwrt/packages/trunk/net/redsocks2 immortalwrt/redsocks2
