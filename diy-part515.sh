9fba3f9a7c0984bbe8703cc4dd9a003face2abab
ayancn <96911869+ayancn@users.noreply.github.com>
2022-06-10 12:44:31 +0800

    Update diy-part515.sh

--- diy-part515.sh
+++ diy-part515.sh
@@ -16,6 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-store kenzok/luci-app-store
 sed -i 's/5.10/5.15/g' target/linux/ramips/Makefile
