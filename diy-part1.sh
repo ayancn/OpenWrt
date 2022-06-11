a9b780cfb4c5ccb401fa2f7699700d8d361dd445
ayancn <96911869+ayancn@users.noreply.github.com>
2022-06-12 04:54:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,7 +18,6 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 git clone https://github.com/jerrykuku/luci-theme-argon package/jerrykuku/luci-theme-argon
 svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-turboacc package/coolsnowwolf/luci-app-turboacc
-svn co https://github.com/fw876/helloworld/trunk/master/luci-app-ssr-plus package/helloworld/luci-app-ssr-plus
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-store package/kenzok/luci-app-store
+svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/helloworld/luci-app-ssr-plus
 sed -i 's/dnsmasq/dnsmasq-full/g' include/target.mk
 sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
