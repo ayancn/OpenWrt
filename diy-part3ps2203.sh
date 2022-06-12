d5ccec623dbc881ea861cb2e4ff37f23184d9e8a
ayancn <96911869+ayancn@users.noreply.github.com>
2022-06-12 18:06:03 +0800

    Update diy-part3ps2203.sh

--- diy-part3ps2203.sh
+++ diy-part3ps2203.sh
@@ -14,16 +14,10 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git lienol https://github.com/Lienol/openwrt-package.git;main' >>feeds.conf.default
-echo 'src-git other https://github.com/Lienol/openwrt-package.git;other' >>feeds.conf.default
-svn co https://github.com/immortalwrt/packages/trunk/net/dns2socks package/immortalwrt/dns2socks
-svn co https://github.com/immortalwrt/packages/trunk/net/microsocks package/immortalwrt/microsocks
-svn co https://github.com/immortalwrt/packages/trunk/net/ipt2socks package/immortalwrt/ipt2socks
-svn co https://github.com/immortalwrt/packages/trunk/net/pdnsd-alt package/immortalwrt/pdnsd-alt
-svn co https://github.com/immortalwrt/packages/trunk/net/redsocks2 package/immortalwrt/redsocks2
-git clone https://github.com/jerrykuku/luci-theme-argon package/jerrykuku/luci-theme-argon
+echo 'src-git immort https://github.com/immortalwrt/packages' >>feeds.conf.default
+echo 'src-git alwrt https://github.com/immortalwrt/luci' >>feeds.conf.default
 svn co https://github.com/linkease/istore/trunk/luci/luci-app-store package/istore/luci-app-store
 sed -i 's/dnsmasq/dnsmasq-full/g' include/target.mk
 sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
