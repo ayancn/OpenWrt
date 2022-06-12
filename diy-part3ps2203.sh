739d45f986b0894f31db84ced64af98c7411c081
ayancn <96911869+ayancn@users.noreply.github.com>
2022-06-12 09:41:01 +0800

    Update diy-part3ps2203.sh

--- diy-part3ps2203.sh
+++ diy-part3ps2203.sh
@@ -21,7 +21,6 @@ svn co https://github.com/immortalwrt/packages/trunk/net/microsocks package/immo
 svn co https://github.com/immortalwrt/packages/trunk/net/ipt2socks package/immortalwrt/ipt2socks
 svn co https://github.com/immortalwrt/packages/trunk/net/redsocks2 package/immortalwrt/redsocks2
 git clone https://github.com/jerrykuku/luci-theme-argon package/jerrykuku/luci-theme-argon
-svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-turboacc package/immortalwrt/luci-app-turboacc
 svn co https://github.com/linkease/istore/trunk/luci/luci-app-store package/istore/luci-app-store
 sed -i 's/dnsmasq/dnsmasq-full/g' include/target.mk
 sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
